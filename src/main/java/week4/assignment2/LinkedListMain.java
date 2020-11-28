1dc497afb8bfb42a6b648bc7241d6a3a9df36f0e
WooCheol Kim <jikimee64@gmail.com>
2020-11-28 22:17:53 +0900

    [20201128] 4주차 코드과제 폴더 및 Main클래스 세팅

---
+++ src/main/java/week4/assignment2/LinkedListMain.java
@@ -0,0 +1,12 @@
+package week4.assignment2;
+
+/**
+ * 과제 2. LinkedList를 구현하세요.
+ * LinkedList에 대해 공부하세요.
+ * 정수를 저장하는 ListNode 클래스를 구현하세요.
+ * ListNode add(ListNode head, ListNode nodeToAdd, int position)를 구현하세요.
+ * ListNode remove(ListNode head, int positionToRemove)를 구현하세요.
+ * boolean contains(ListNode head, ListNode nodeTocheck)를 구현하세요.
+ */
+public class LinkedListMain {
+}
---
+++ src/main/java/week4/assignment5/QueueArrayMain.java
@@ -0,0 +1,8 @@
+package week4.assignment5;
+/**
+ * (optional) 과제 5. Queue를 구현하세요.
+ * 배열을 사용해서 한번
+ * ListNode를 사용해서 한번.
+ */
+public class QueueArrayMain {
+}
